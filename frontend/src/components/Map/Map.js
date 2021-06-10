5244740d3b3156ee99d6c66025305fc07127de00
Mar <mathew.brown@hakai.org>
2021-06-10 08:42:37 -0700

    added bathymetry tiles

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -3,7 +3,10 @@ import mapboxgl from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 
 export default function createMap() {
-  var map = new Map({
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiaGFrYWkiLCJhIjoiY2lyNTcwYzY5MDAwZWc3bm5ubTdzOWtzaiJ9.6QhxH6sQEgK634qO7a8MoQ';
+
+  var map = new mapboxgl.Map({
     container: "map",
     style: {
       version: 8,
@@ -29,7 +32,33 @@ export default function createMap() {
 
   map.on("load", function () {
     console.log("test");
-
+    map.addSource('10m-bathymetry-81bsvj', {
+      type: 'vector',
+      url: 'mapbox://mapbox.9tm8dx88'
+      });
+    map.addLayer(
+      {
+      'id': '10m-bathymetry-81bsvj',
+      'type': 'fill',
+      'source': '10m-bathymetry-81bsvj',
+      'source-layer': '10m-bathymetry-81bsvj',
+      'layout': {},
+      'paint': {
+      'fill-outline-color': 'hsla(337, 82%, 62%, 0)',
+      // cubic bezier is a four point curve for smooth and precise styling
+      // adjust the points to change the rate and intensity of interpolation
+      'fill-color': [
+      'interpolate',
+      ['cubic-bezier', 0, 0.5, 1, 0.5],
+      ['get', 'DEPTH'],
+      200,
+      '#78bced',
+      9000,
+      '#15659f'
+      ]
+      }
+      },
+      );
     map.addLayer({
       id: "internal-layer-name",
       type: "circle",
@@ -45,6 +74,8 @@ export default function createMap() {
         "circle-opacity": 0.75,
       },
     });
+
+   
   });
 
   const drawPolygon = new MapboxDraw();
