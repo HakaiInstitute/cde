d41312ab94714b22af4b44db9a155ef3ff0a5d91
Jessy Barrette <jessy.barrette@hakai.org>
2021-06-16 17:27:14 -0700

    Add an optional input to the __main__ scraper method avoid threading

--- scraper/erddap_scraper/__main__.py
+++ scraper/erddap_scraper/__main__.py
@@ -7,20 +7,25 @@ import pandas as pd
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("erddap_urls")
+    parser.add_argument("--threading", default="True")
     args = parser.parse_args()
     erddap_urls=args.erddap_urls
+    threading = args.threading
 
     threads=[]
     result=[]
 
     for erddap_url in erddap_urls.split(','):
             print("Starting scraper:",erddap_url)
-            scraping_thread = threading.Thread(target=scrape_erddap, args=(erddap_url,result))
-            scraping_thread.start()
-            threads.append(scraping_thread)
-
-    for thread in threads:
-        thread.join()
+            if threading == 'True':
+                scraping_thread = threading.Thread(target=scrape_erddap, args=(erddap_url,result))
+                scraping_thread.start()
+                threads.append(scraping_thread)
+            else:
+                result = scrape_erddap(erddap_url, result)
+    if threading:
+        for thread in threads:
+            thread.join()
 
     profiles=pd.DataFrame()
     datasets=pd.DataFrame()
