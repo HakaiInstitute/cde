e91265dc80a6489993d8f63660d0843c36730bd0
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-05-23 11:13:00 -0400

    add harvest config as parameter within input and provide parameter input in caprover deployment

--- harvester/run.sh
+++ harvester/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python -m cde_harvester -f harvest_config.yaml && python -m cde_db_loader
+python -m cde_harvester -f ${HARVEST_CONFIG_FILE:-harvest_config.yaml} && python -m cde_db_loader
